0a50ac07f69938b0c074ba699559b820b509a8a6
pyro3x <pyro3x@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-10 05:18:46 +0000

    bugfixes;display current image - admin
    git-svn-id: https://plugins.svn.wordpress.org/wpcustom-category-image/trunk@1041675 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/add-form-option-image.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+<div class="form-field wpcustom-category-form-field">
+	<label for="taxonomy_image"><?php echo $label['image']; ?></label>
+
+	<input type="hidden" name="categoryimage_attachment" id="categoryimage_attachment" value="<?php echo $categoryimage_attachment; ?>">
+
+	<div id="categoryimage_imageholder">
+		<?php if(isset($categoryimage_image)): ?>
+			<img src="<?php echo $categoryimage_image; ?>" width="180" id="categoryimage_image" />
+		<?php endif; ?>
+	</div>
+
+	<div class="options">
+		<button class="button" id="categoryimage_upload_button"><?php echo $label['upload_image']; ?></button>
+		<button class="button" id="categoryimage_remove_button"><?php echo $label['remove_image']; ?></button>
+	</div>
+</div>
